91b96d48d7087ec7cd6acaccf3b11d4474861ec1
Wyatt Sheffield <cws@wyatts.xyz>
2022-07-31 16:10:58 -0500

    Showing total work done followed by a simulation

--- field.py
+++ field.py
@@ -1,18 +1,23 @@
+# flake8: noqa
 from manim import *
+import numpy as np
 
 FORCE_FIELDS = {
         "assigned": lambda pos: (pos[0] * pos[0] * RIGHT) + (pos[0] * pos[1] * UP),
+        "assigned-shift": lambda pos: (RIGHT * (pos[0] + 1)**2) + ((pos[0]+1) * (pos[1]+1) * UP),
         "textbook": lambda pos: (pos[0] * pos[0] * RIGHT) - (pos[0] * pos[1] * UP)
         }
 
 FIELD_EQNS = {
         "assigned": MathTex(r"\mathbf{F}(x,y)=x^2\hat{\imath} + xy\hat{\jmath}"),
+        "assigned-shift": MathTex(r"\mathbf{F}(x,y)=(x+1)^2\hat{\imath} + (x+1)(y+1)\hat{\jmath}"),
         "textbook": MathTex(r"\mathbf{F}(x,y)=x^2\hat{\imath} - xy\hat{\jmath}")
         }
 
 CIRCLE_EQNS = {
         "implicit": MathTex(r"x^2 + y^2 = 2^2"),
-        "parametric": MathTex(r"\overrightarrow{\mathbf{r}}(t) = \langle 2\cos{t}, 2\sin{t} \rangle")
+        "parametric": MathTex(r"\overrightarrow{\mathbf{r}}(t) = \langle 2\cos{t}, 2\sin{t} \rangle"),
+        "parametric-shift": MathTex(r"\overrightarrow{\mathbf{r}}(t)=\langle 1 + 2\cos{t}, 1 + 2\sin{t}\rangle")
         }
 
 RADIUS = 2.0
@@ -20,13 +25,16 @@ RADIUS = 2.0
 class PointMass(Dot):
     def set_vel(self, velocity):
         self.vel = np.array(velocity)
+
     def set_pos(self, position):
         self.pos = np.array([position[0], position[1], 0.0])
         self.set_x(position[0])
         self.set_y(position[1])
         self.set_z(0)
+
     def get_vel(self):
         return self.vel
+
     def get_pos(self):
         return self.pos
 
@@ -45,11 +53,11 @@ class LineIntegral(MovingCameraScene):
 
 
         # replace vector field with stream lines
-        #stream_lines = StreamLines(force_field, stroke_width=2, max_anchors_per_line=30,
-        #        max_color_scheme_value=np.linalg.norm(force_field([config.frame_width/2,config.frame_height/2])))
-        #stream_lines.set_z_index(-1)
-        #self.add(stream_lines)
-        #stream_lines.start_animation(warm_up=False, flow_speed=1.5)
+        stream_lines = StreamLines(force_field, stroke_width=2, max_anchors_per_line=30,
+                max_color_scheme_value=np.linalg.norm(force_field([config.frame_width/2,config.frame_height/2])))
+        stream_lines.set_z_index(-1)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=1.5)
         self.play(Uncreate(vf))
 
         # create circle and point
@@ -57,57 +65,55 @@ class LineIntegral(MovingCameraScene):
         point = PointMass()
         CIRCLE_EQNS["implicit"].next_to(circ, DOWN)
         CIRCLE_EQNS["parametric"].next_to(circ, DOWN)
+        CIRCLE_EQNS["parametric-shift"].next_to(circ, DOWN)
         self.play(Create(circ), Write(CIRCLE_EQNS["implicit"]))
         self.wait(2)
 
         point.set_vel([0.0, 10.0, 0.0])
         point.set_pos([RADIUS, 0.0, 0])
-        #point.add_updater(lambda mob, dt: self.verlet(mob, dt, force_field, substeps=10))
         self.add(point)
-        self.play(Transform(CIRCLE_EQNS["implicit"], CIRCLE_EQNS["parametric"]), MoveAlongPath(point, circ))
+        self.play(ReplacementTransform(CIRCLE_EQNS["implicit"], CIRCLE_EQNS["parametric"]), MoveAlongPath(point, circ))
 
-        self.play(self.camera.frame.animate.shift(LEFT*4), field_desc.animate.shift(LEFT*4))
-
+        self.play(self.camera.frame.animate.shift(LEFT*3.5), field_desc.animate.shift(LEFT*3.5))
 
         # add axes for integral graph
-        riemann_group = VGroup()
-        scale_factor = 3/5
+        scale_factor = 13/(4.5*PI*2)
         riemann_axes = Axes(
-                x_range=[0, 4.5*PI, PI/2], y_range=[-4,4,1],
-                x_length=8, #y_length=6,
+                x_range=[0, 4.5*PI, PI/2], y_range=[-4, 4, 1],
+                #x_length=8, y_length=6,
                 x_axis_config={
                     "include_ticks": True,
                     "unit_size": PI/2,
                     }
         )
-        #riemann_axes.add_x_labels({
-        #    PI/2:   MathTex(r"\frac{\pi}{2}"),
-        #    PI:     MathTex(r"\pi"),
-        #    3*PI/2: MathTex(r"\frac{3\pi}{2}"),
-        #    PI*2:   MathTex(r"2\pi")
-        #    }
-        #)
+        # riemann_axes.add_x_labels({
+        #     PI/2:   MathTex(r"\frac{\pi}{2}"),
+        #     PI:     MathTex(r"\pi"),
+        #     3*PI/2: MathTex(r"\frac{3\pi}{2}"),
+        #     PI*2:   MathTex(r"2\pi")
+        #     }
+        # )
         riemann_axes.scale(scale_factor)
-        riemann_axes.move_to([-7,0,0])
+        riemann_axes.move_to([-7, 0, 0])
         self.add(riemann_axes)
-
 
         def run_integral_vis(steps=10):
             time_around_circle = 3 # seconds
             path_length = 2 * PI
             theta = path_length / steps
             point.add_updater(lambda x: x.set_pos(x.get_center()))
-            gradient = color_gradient([GREEN, BLUE], steps)
             rectangles = []
             accumulated_work = 0
             accumulated_dist = 0
             plot_points = []
+            gradient = color_gradient([GREEN, BLUE], steps)
             for i in range(steps):
                 progress = i / steps
                 begin = point.get_pos()
                 self.play(MoveAlongPath(point, circ),
                         run_time=time_around_circle/steps,
-                        rate_func=lambda x:progress + x/steps)
+                        rate_func=lambda x:min(1,progress + x/steps)
+                )
                 work, vertex_list = self.line_integral_partition(
                         begin, point.get_pos(),
                         force_field
@@ -123,44 +129,87 @@ class LineIntegral(MovingCameraScene):
                 rect = Polygon(*rect_verts).set_fill(gradient[i], 0.85).set_stroke(width=0)
                 rectangles.append(rect)
                 riemann_axes.add(rect)
+
             self.wait(2)
             integral_plot = riemann_axes.plot_line_graph(
                     [x[0] for x in plot_points], [x[1] for x in plot_points],
                     add_vertex_dots=False
                     )
-            self.play(Write(integral_plot))
+            work_label = Variable(accumulated_work, "Total Work").next_to(riemann_axes, UP)
+            self.play(Write(integral_plot), Write(work_label))
             self.wait(2)
-            self.play(Unwrite(integral_plot))
+            self.play(Unwrite(integral_plot), Unwrite(work_label))
             riemann_axes.remove(integral_plot)
             plot_points=[]
             print(accumulated_dist, accumulated_work)
             riemann_axes.remove(*rectangles)
             point.clear_updaters()
+            return accumulated_work
 
-        #run_integral_vis(10)
+        run_integral_vis(10)
         #run_integral_vis(50)
-        run_integral_vis(50)
-        self.play(circ.animate.shift(UP+RIGHT), point.animate.shift(UP+RIGHT))
+        #run_integral_vis(20)
+        #run_integral_vis(100)
+        self.play(
+            circ.animate.shift(UP+RIGHT),
+            point.animate.shift(UP+RIGHT),
+            ReplacementTransform(CIRCLE_EQNS["parametric"], CIRCLE_EQNS["parametric-shift"])
+        )
         point.set_pos(point.get_center())
-        run_integral_vis(50)
+        run_integral_vis(10)
+        energy = run_integral_vis(150)
         self.wait(3)
 
+        # Setup for Verlet simulation
 
-    def verlet(self, mob, dt, force_field, substeps = 1, radius=2):
+        self.play(Uncreate(stream_lines), Uncreate(riemann_axes))
+        self.play(self.camera.frame.animate.shift(RIGHT*3.5),
+                field_desc.animate.shift(RIGHT*3.5))
+
+        chosen_field = "assigned-shift"
+        force_field  = FORCE_FIELDS[chosen_field]
+        stream_lines = StreamLines(force_field, stroke_width=2, max_anchors_per_line=30,
+                max_color_scheme_value=np.linalg.norm(force_field([config.frame_width/2,config.frame_height/2])))
+        stream_lines.set_z_index(-1)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=1.5)
+        self.play(ReplacementTransform(field_desc, FIELD_EQNS[chosen_field]))
+
+        self.play(
+            circ.animate.shift(-1*(UP+RIGHT)),
+            point.animate.shift(-1*(UP+RIGHT)),
+            ReplacementTransform(CIRCLE_EQNS["parametric-shift"], CIRCLE_EQNS["parametric"])
+        )
+        point.set_pos(point.get_center())
+        point.set_vel([0., 0., 0.])
+        point.add_updater(lambda mob, dt: self.verlet(mob, dt, force_field, substeps=10))
+        self.wait(10)
+        point.clear_updaters()
+        point.move_to([1.,0.,0.])
+        point.set_vel([0., -1 * (2*energy)**0.5, 0.])
+        point.add_updater(lambda mob, dt: self.verlet(mob, dt, force_field, substeps=10))
+        self.wait(10)
+
+    def verlet(self, mob, dt, force_field, substeps=1, radius=2, drag_coef=1.0):
         """
         Verlet integrator - Calculate force on the point and update its
         position and velocity accordingly.
 
-        mob: Mobject - the point moving around the circle
-        dt: Time since this function was last called
-        force_field: Vector force field experienced by the object
-        substeps: Higher value increases the accuracy of the integration
-        radius: Radius of the constraining circle
+        mob:            Mobject - the point moving around the circle
+        dt:             Time since this function was last called
+        force_field:    Vector force field experienced by the object
+        substeps:       Higher value increases the accuracy of the integration
+        radius:         Radius of the constraining circle
+        drag_coef:      A value of 1 indicates no drag whatsoever. A reasonable
+                        value for slight drag is 0.999: Assuming 60fps with
+                        substeps=1, a particle with an initial velocity of 100
+                        would slow to a velocity of 94 after 1 second, down to
+                        74 after 5 seconds.
         """
         dt /= substeps
         pos = mob.get_pos()
         vel = mob.get_vel()
-        for i in range(substeps):
+        for _ in range(substeps):
             pos += dt * vel
             # constrain position to the circle
             pos = radius * pos / np.linalg.norm(pos)
@@ -170,7 +219,8 @@ class LineIntegral(MovingCameraScene):
             tangent = np.array([-1 * pos[1], pos[0], 0.0])
             tangent /= np.linalg.norm(tangent) #normalized to magnitude 1
             vel = np.dot(vel, tangent) * tangent
-            vel *= 0.999 # drag
+            vel *= drag_coef
+
         mob.set_pos(pos)
         mob.set_vel(vel)
         return mob
@@ -196,10 +246,4 @@ class LineIntegral(MovingCameraScene):
                 [0, height]   # left top
         ])
         return work, vertex_list
-
-    def full_rotation(self, mob, radius, steps, force_field):
-        radians = 2 * PI / steps
-        for i in range(steps):
-            work, old_pos, new_pos = move_around_circle(mob, radius, radians, force_field)
-
 
